eea8c817b96ba4d1347adf38685bb48d62ca155b
Jarrod Luca <jarrodluca@gmail.com>
2017-03-30 12:03:15 -0400

    add second rover and adjust functions

--- rover.js
+++ rover.js
@@ -14,7 +14,7 @@ var myRover = {
 };
 
 var myRover2 = {
-  position: [10,10],
+  position: [0,10],
   direction:'S'
 };
 
@@ -37,18 +37,20 @@ var minLat = gridArray[0][0];
 var minLong = gridArray[1][0];
 
 // prints position and direction to console and html page
-function printData(rover) {
-  console.log("\nNew Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]");
-  console.log("Current direction: " + rover.direction);
+function printData(rover, rover2) {
+  // console.log("\nNew Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  // console.log("Current direction: " + rover.direction);
   var output = document.getElementById("output");
-  output.innerHTML = ("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]" +
-                      "<br>Current direction: " + myRover.direction);
-console.log(myRover.position);
-console.log(obstacle.position);
+  output.innerHTML = ("Rover1 Position: [" + rover.position[0] + ", " + rover.position[1] + "]" +
+                      "<br>Rover1 current direction: " + rover.direction + "<br><br>Rover2 Position: [" + rover2.position[0] + ", " + rover2.position[1] + "]"
+                    + "<br>Rover2 current direction: " + rover2.direction);
+// console.log(myRover.position);
+// console.log(myRover2.position);
+// console.log(obstacle.position);
 }
 
 //turn function
-function move(rover) {
+function move(rover, rover2) {
   var myCommands = document.getElementById('command').value.toUpperCase();
     for (var i = 0; i < myCommands.length; i++) {
       switch(myCommands[i]) {
@@ -69,6 +71,22 @@ function move(rover) {
             rover.direction = 'S';
             console.log("Turning: " + rover.direction);
           }
+          if (rover2.direction === 'N') {
+            rover2.direction = 'W';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'E') {
+            rover2.direction = 'N';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'S') {
+            rover2.direction = 'E';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'W') {
+            rover2.direction = 'S';
+            console.log("Turning: " + rover2.direction);
+          }
         break;
 
         case 'R':
@@ -88,112 +106,208 @@ function move(rover) {
             rover.direction = 'N';
             console.log("Turning: " + rover.direction);
           }
+          if (rover2.direction === 'N') {
+            rover2.direction = 'E';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'E') {
+            rover2.direction = 'S';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'S') {
+            rover2.direction = 'W';
+            console.log("Turning: " + rover2.direction);
+          }
+          else if (rover2.direction === 'W') {
+            rover2.direction = 'N';
+            console.log("Turning: " + rover2.direction);
+          }
         break;
 
         case 'F':
-          moveForward(myRover);
+          moveForward(myRover,myRover2);
         break;
 
         case 'B':
-          moveBackward(myRover);
+          moveBackward(myRover,myRover2);
         break;
       }
-   }
-   obstacleCheck();
-   printData(myRover);
+    }
+   obstacleCheck(myRover, myRover2);
+   printData(myRover, myRover2);
 }
 
-function moveForward(rover) {
+function moveForward(rover, rover2) {
   switch(rover.direction) {
     case 'N':
-      if (myRover.position[1] < maxLong) {
+      if (rover.position[1] < maxLong) {
         // longitude++;
-        myRover.position[1]++;
+        rover.position[1]++;
       }
       else {
-        myRover.position[1] = minLong;
+        rover.position[1] = minLong;
       }
       break;
 
     case 'E':
-      if (myRover.position[0] < maxLat) {
-        myRover.position[0]++;
+      if (rover.position[0] < maxLat) {
+        rover.position[0]++;
       }
       else {
-        myRover.position[0] = minLat;
+        rover.position[0] = minLat;
       }
     break;
 
     case 'S':
-      if (myRover.position[1] > minLong) {
-        myRover.position[1]--;
+      if (rover.position[1] > minLong) {
+        rover.position[1]--;
       }
       else {
-        myRover.position[1] = maxLong;
+        rover.position[1] = maxLong;
       }
     break;
 
     case 'W':
-      if (myRover.position[0] > minLat) {
-        myRover.position[0]--;
+      if (rover.position[0] > minLat) {
+        rover.position[0]--;
       }
       else {
-        myRover.position[0] = maxLat;
+        rover.position[0] = maxLat;
+      }
+    break;
+  }
+  switch(rover2.direction) {
+    case 'N':
+      if (rover2.position[1] < maxLong) {
+        // longitude++;
+        rover2.position[1]++;
+      }
+      else {
+        rover2.position[1] = minLong;
+      }
+      break;
+
+    case 'E':
+      if (rover2.position[0] < maxLat) {
+        rover2.position[0]++;
+      }
+      else {
+        rover2.position[0] = minLat;
+      }
+    break;
+
+    case 'S':
+      if (rover2.position[1] > minLong) {
+        rover2.position[1]--;
+      }
+      else {
+        rover2.position[1] = maxLong;
+      }
+    break;
+
+    case 'W':
+      if (rover2.position[0] > minLat) {
+        rover2.position[0]--;
+      }
+      else {
+        rover2.position[0] = maxLat;
       }
     break;
   }
 }
 
-function moveBackward(rover) {
+function moveBackward(rover, rover2) {
   switch(rover.direction) {
     case 'N':
-      if (myRover.position[1] > minLong) {
-        myRover.position[1]--;
+      if (rover.position[1] > minLong) {
+        rover.position[1]--;
       }
       else {
-        myRover.position[1] = maxLong;
+        rover.position[1] = maxLong;
       }
     break;
     case 'E':
-      if (myRover.position[0] > minLat) {
-        myRover.position[0]--;
+      if (rover.position[0] > minLat) {
+        rover.position[0]--;
       }
       else {
-        myRover.position[0] = maxLat;
+        rover.position[0] = maxLat;
       }
     break;
     case 'S':
-      if (myRover.position[1] < maxLong) {
-        myRover.position[1]++;
+      if (rover.position[1] < maxLong) {
+        rover.position[1]++;
       }
       else {
-        myRover.position[1] = minLong;
+        rover.position[1] = minLong;
       }
     break;
     case 'W':
-      if (myRover.position[0] < maxLat) {
-        myRover.position[0]++;
+      if (rover.position[0] < maxLat) {
+        rover.position[0]++;
       }
       else {
-        myRover.position[0] = minLat;
+        rover.position[0] = minLat;
+      }
+    break;
+  }
+  switch(rover2.direction) {
+    case 'N':
+      if (rover2.position[1] > minLong) {
+        rover2.position[1]--;
+      }
+      else {
+        rover2.position[1] = maxLong;
+      }
+    break;
+    case 'E':
+      if (rover2.position[0] > minLat) {
+        rover2.position[0]--;
+      }
+      else {
+        rover2.position[0] = maxLat;
+      }
+    break;
+    case 'S':
+      if (rover2.position[1] < maxLong) {
+        rover2.position[1]++;
+      }
+      else {
+        rover2.position[1] = minLong;
+      }
+    break;
+    case 'W':
+      if (rover2.position[0] < maxLat) {
+        rover2.position[0]++;
+      }
+      else {
+        rover2.position[0] = minLat;
       }
     break;
   }
 }
 
-function reset(rover) {
-  myRover.position[0] = 0;
-  myRover.position[1] = 0;
+function reset(rover,rover2) {
+  rover.position[0] = 0;
+  rover.position[1] = 0;
   rover.direction = 'N';
+  rover2.position[0] = 0;
+  rover2.position[1] = 10;
+  rover2.direction = 'S';
   document.getElementById("command").value = "";
-  printData(myRover);
+  printData(myRover, myRover2);
 }
 
-function obstacleCheck() {
-  var rov = myRover.position.toString();
+function obstacleCheck(rover, rover2) {
+  var rov = rover.position.toString();
+  var rov2 = rover2.position.toString();
   var obs = obstacle.position.toString();
-  if (rov === obs) {
+  if (rov === obs || rov2 === obs) {
     alert("Obstacle detected. \n Resetting rover...");
-    reset(myRover);
+    reset(myRover, myRover2);
+  }
+  if (rov === rov2) {
+    alert("Rover collision.\nResetting rovers...");
+    reset(myRover,myRover2);
   }
 }
